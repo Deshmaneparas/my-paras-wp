9d219c01986ddd0eb0dcb31e2d4fb445c0e59cb4
Deshmaneparas <141109408+Deshmaneparas@users.noreply.github.com>
2023-08-08 15:36:17 +0530

    5 files modified

--- src/main/sidenav/chats/chats.component.ts
+++ src/main/sidenav/chats/chats.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatComponent } from './chat/chat.component';
 
 @Component({
   standalone:true,
   selector: 'app-chats',
   templateUrl: './chats.component.html',
   styleUrls: ['./chats.component.css'],
-  imports:[ChatsComponent],
+  imports:[ChatComponent]
 })
 export class ChatsComponent implements OnInit {
 
